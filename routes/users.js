cb245b6280d37719a36d5de4f5e4d86f130a3796
lwi <louis.delfieux@gmail.com>
2017-10-07 09:28:35 +0200

    Add apidoc to project

--- routes/users.js
+++ routes/users.js
@@ -26,3 +26,39 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+/**
+ * @api {get} /user Request User Presentation Page
+ * @apiName GetUserBasicRequest
+ * @apiGroup User
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "author":{
+ *              "firstName":"Jean",
+ *              "lastName":"Dupond",
+ *              "email":"jean.dupond@gmail.com",
+ *              "address":{
+ *                  "rue":"9 rue de la Paix",
+ *                  "city":"Paris",
+ *                  "postal_code":"75002"
+ *                  },
+ *              "phone_number":"0683165620",
+ *          "coordinate":{
+ *              "long":2.331241099999943,
+ *              "lat":48.8687607
+ *              }
+ *          },
+ *          "created":1457910000
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
